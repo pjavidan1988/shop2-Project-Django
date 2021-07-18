082c57d37842249ea6d0d9ae7afe1080a300e705
peyman <p.javidan1988@gmail.com>
2021-07-18 23:16:57 +0430

    7/18/2021 23:16

--- doorsunmagnetshop/urls.py
+++ doorsunmagnetshop/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,5 +22,9 @@ urlpatterns = [
     path('', include('home.urls')),
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/models.py
+++ product/models.py
@@ -1,7 +1,9 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
-
 # Create your models here.
+from django.utils.safestring import mark_safe
+
 
 class Category(models.Model):
     STATUS = (
@@ -11,7 +13,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     keywords = models.CharField(max_length=255)
-    description = models.TextField(max_length=255)
+    description = RichTextUploadingField()
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
@@ -31,15 +33,30 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
-    description = models.TextField(max_length=255)
-    image=models.ImageField(blank=True, upload_to='images/')
+    description = RichTextUploadingField()
+    image = models.ImageField(upload_to='images/')
     price = models.FloatField()
-    amount=models.IntegerField()
-    minamount=models.IntegerField()
-    detail=models.TextField()
+    amount = models.IntegerField()
+    minamount = models.IntegerField()
+    detail = RichTextUploadingField()
     slug = models.SlugField()
-    status=models.CharField(max_length=10,choices=STATUS)
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Images'
+
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
     def __str__(self):
         return self.title
