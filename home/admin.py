64b83693cfb7953783fd1194f4971bcb7bb9d523
peyman <p.javidan1988@gmail.com>
2021-07-19 18:51:02 +0430

    7/19/2021 23:20

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import Setting
+
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ['title','company', 'update_at','status']
+
+
+admin.site.register(Setting,SettingAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,40 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
+
 # Create your models here.
+
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = RichTextUploadingField(blank=True)
+    company = models.CharField(max_length=50)
+    address = RichTextUploadingField(blank=True)
+    phone = models.CharField(blank=True, max_length=15)
+    fax = models.CharField(blank=True, max_length=15)
+    email = models.CharField(blank=True, max_length=50)
+    smtp_server = models.CharField(blank=True, max_length=50)
+    smtp_email = models.CharField(blank=True, max_length=50)
+    smtp_password = models.CharField(blank=True, max_length=10)
+    smtp_port = models.CharField(blank=True, max_length=5)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    logo = models.ImageField(blank=True, upload_to='images/')
+    facebook = models.CharField(blank=True, max_length=50)
+    instagram = models.CharField(blank=True, max_length=50)
+    twitter = models.CharField(blank=True, max_length=50)
+    youtube = models.CharField(blank=True, max_length=50)
+    whatsapp = models.CharField(blank=True, max_length=50)
+    telegram = models.CharField(blank=True, max_length=50)
+    aboutus = RichTextUploadingField(blank=True)
+    contact = RichTextUploadingField(blank=True)
+    references = RichTextUploadingField(blank=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
